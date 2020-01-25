067065be204cee577f233f5bb2c4afc638375935
Architect38 <gothic38101@gmail.com>
2020-01-25 20:24:09 +0400

    Updates

---
+++ precache-manifest.81cc62ec955c1c9cdc80f3a4153b4b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee3bfcb5798078aae77832dd4601eaa",
+    "url": "/Sushi/index.html"
+  },
+  {
+    "revision": "8f5564be3ec950737247",
+    "url": "/Sushi/static/css/main.f0b166d5.chunk.css"
+  },
+  {
+    "revision": "1e759ef98ed4a78f1651",
+    "url": "/Sushi/static/js/2.0f15b1c0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Sushi/static/js/2.0f15b1c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f5564be3ec950737247",
+    "url": "/Sushi/static/js/main.d526ac7d.chunk.js"
+  },
+  {
+    "revision": "6b7ec8cfa3d3b46612ba",
+    "url": "/Sushi/static/js/runtime-main.5bfa0981.js"
+  }
+]);
