ebd767f8a1ba1399b8196704f8f272d8ab31d8a3
Architect38 <gothic38101@gmail.com>
2020-01-25 20:26:22 +0400

    created links

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <div className="app">
         <Header/>
-        <Route exact path="/" render={()=><MainScreen/>} />
+        <Route exact path="/Sushi/" render={()=><MainScreen/>} />
         <Route exact path="/catalog" render={()=><div></div>} />
         <Footer/>
       </div>
