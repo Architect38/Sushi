105ccdf2f1ca6851075002f745d5d538e28a63ea
Architect38 <gothic38101@gmail.com>
2020-01-26 20:39:13 +0400

    added link for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
     render() {
       return (
         <div className={s.main}>
-          <img className={s.logo} src="https://xn--80adjkr6adm9b.xn--p1ai/static/sv/img/logo.png"/>
+          <NavLink className={s.redirect} to="/sushi">
+            <img className={s.logo} src="https://xn--80adjkr6adm9b.xn--p1ai/static/sv/img/logo.png"/>
+          </NavLink>
           <span className={s.info_1}>
               <div><span>Волгоград&#9660;</span><span>8-800-550-30-30</span></div>
               <div><span>Личный кабинет</span></div>
