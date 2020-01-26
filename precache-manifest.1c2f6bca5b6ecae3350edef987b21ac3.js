305846799aa76ac8e9a1470ac98808d14b96f7c4
Architect38 <gothic38101@gmail.com>
2020-01-26 22:26:20 +0400

    Updates

---
+++ precache-manifest.1c2f6bca5b6ecae3350edef987b21ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb91576030e718874b6b2a034146bbfe",
+    "url": "/Sushi/index.html"
+  },
+  {
+    "revision": "259a2e73f3adda344f29",
+    "url": "/Sushi/static/css/main.6da6ab6a.chunk.css"
+  },
+  {
+    "revision": "bbc982b74307d7bf6497",
+    "url": "/Sushi/static/js/2.595b0292.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Sushi/static/js/2.595b0292.chunk.js.LICENSE"
+  },
+  {
+    "revision": "259a2e73f3adda344f29",
+    "url": "/Sushi/static/js/main.4fc7554b.chunk.js"
+  },
+  {
+    "revision": "6b7ec8cfa3d3b46612ba",
+    "url": "/Sushi/static/js/runtime-main.5bfa0981.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sushi/precache-manifest.fdc26050f81c3d8d22d06a5537e5e728.js"
+  "/Sushi/precache-manifest.1c2f6bca5b6ecae3350edef987b21ac3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sushi/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sushi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
